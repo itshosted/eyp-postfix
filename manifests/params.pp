a35de19ef48cba70475281317952b39ad003cfd7
Jethro van Ginkel <jethro@itshosted.nl>
2026-05-31 18:47:26 +0200

    .

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,201 +1,184 @@
 #
 class postfix::params {
-
   $package_name='postfix'
   $baseconf = '/etc/postfix'
 
   # $package_postfix_utils = 'postfix-perl-scripts'
 
-  case $::osfamily
-  {
+  case $facts['os']['family'] {
     'redhat':
-    {
-      $setgid_group_default='postdrop'
-      $compatibility_level_default=undef
-
-      $daemon_directory_default='/usr/libexec/postfix'
-      #$dependencies=['chkconfig', 'grep']
-      $switch_to_postfix='alternatives --set mta /usr/sbin/sendmail.postfix'
-      $check_postfix_mta='alternatives --display mta | grep postfix'
-
-      $purge_default_mta=[ 'exim', 'sendmail' ]
-
-      $mailclient=[ 'mailx' ]
-
-      $readme_directory_default = false
-
-      $postfix_username_uid_default='89'
-      $postfix_username_gid_default='89'
-
-      case $::operatingsystemrelease
       {
-        /^5.*$/:
-        {
-          $manage_mastercf_default=false
-          $postfix_ver='2.3.3'
-        }
-        /^6.*$/:
-        {
-          $manage_mastercf_default=true
-          $postfix_ver='2.6.6'
-        }
-        /^7.*$/:
-        {
-          $manage_mastercf_default=true
-          $postfix_ver='2.10.1'
-        }
-        /^8.*$/:
-        {
-          $manage_mastercf_default=true
-          $postfix_ver='3.3.1'
-        }
-        default: { fail('Unsupported RHEL/CentOS version!')  }
-      }
-    }
-    'Debian':
-    {
-      case $::operatingsystem
-      {
-        'Ubuntu':
-        {
-          $manage_mastercf_default=false
-          $setgid_group_default='postdrop'
+        $setgid_group_default='postdrop'
+        $compatibility_level_default=undef
 
-          $switch_to_postfix=undef
-          $check_postfix_mta=undef
+        $daemon_directory_default='/usr/libexec/postfix'
+        #$dependencies=['chkconfig', 'grep']
+        $switch_to_postfix='alternatives --set mta /usr/sbin/sendmail.postfix'
+        $check_postfix_mta='alternatives --display mta | grep postfix'
 
-          $purge_default_mta=undef
+        $purge_default_mta= ['exim', 'sendmail']
 
-          $mailclient=[ 'mailutils' ]
+        $mailclient= ['mailx']
 
-          $readme_directory_default='/usr/share/doc/postfix'
+        $readme_directory_default = false
 
-          if($::facts!=undef)
+        $postfix_username_uid_default='89'
+        $postfix_username_gid_default='89'
+
+        case $facts['os']['release']['full'] {
+          /^5.*$/:
           {
-            if 'eyp_postfix_uid' in $::facts
-            {
-              # $postfix_username_uid_default=hiera('::eyp_postfix_uid', '89'),
-              $postfix_username_uid_default = $::facts['eyp_postfix_uid'] ? {
+            $manage_mastercf_default=false
+            $postfix_ver='2.3.3'
+          }
+          /^6.*$/:
+          {
+            $manage_mastercf_default=true
+            $postfix_ver='2.6.6'
+          }
+          /^7.*$/:
+          {
+            $manage_mastercf_default=true
+            $postfix_ver='2.10.1'
+          }
+          /^8.*$/:
+          {
+            $manage_mastercf_default=true
+            $postfix_ver='3.3.1'
+          }
+          default: { fail('Unsupported RHEL/CentOS version!') }
+        }
+      }
+      'Debian':
+      {
+        case $facts['os']['name'] {
+          'Ubuntu':
+          {
+            $manage_mastercf_default=false
+            $setgid_group_default='postdrop'
+
+            $switch_to_postfix=undef
+            $check_postfix_mta=undef
+
+            $purge_default_mta=undef
+
+            $mailclient= ['mailutils']
+
+            $readme_directory_default='/usr/share/doc/postfix'
+
+            if($::facts!=undef) {
+              if 'eyp_postfix_uid' in $::facts {
+                # $postfix_username_uid_default=hiera('::eyp_postfix_uid', '89'),
+                $postfix_username_uid_default = $::facts['eyp_postfix_uid'] ? {
+                  undef   => '89',
+                  default => $::facts['eyp_postfix_uid'],
+                }
+              } else {
+                $postfix_username_uid_default = '89'
+              }
+
+              if 'eyp_postfix_gid' in $::facts {
+                # $postfix_username_gid_default=hiera('::eyp_postfix_gid', '89'),
+                $postfix_username_gid_default = $::facts['eyp_postfix_gid'] ? {
+                  undef   => '89',
+                  default => $::facts['eyp_postfix_gid'],
+                }
+              } else {
+                $postfix_username_gid_default = '89'
+              }
+            } else {
+              $postfix_username_uid_default = $facts['eyp_postfix_uid'] ? {
                 undef   => '89',
-                default => $::facts['eyp_postfix_uid'],
+                default => $facts['eyp_postfix_uid'],
+              }
+              $postfix_username_gid_default = $facts['eyp_postfix_gid'] ? {
+                undef   => '89',
+                default => $facts['eyp_postfix_gid'],
               }
             }
-            else
-            {
-              $postfix_username_uid_default = '89'
-            }
 
-            if 'eyp_postfix_gid' in $::facts
-            {
-              # $postfix_username_gid_default=hiera('::eyp_postfix_gid', '89'),
-              $postfix_username_gid_default = $::facts['eyp_postfix_gid'] ? {
-                undef   => '89',
-                default => $::facts['eyp_postfix_gid'],
+            case $facts['os']['release']['full'] {
+              /^14.*$/:
+              {
+                $daemon_directory_default='/usr/lib/postfix'
+                $postfix_ver='2.11.0'
+                $compatibility_level_default=undef
               }
-            }
-            else
-            {
-              $postfix_username_gid_default = '89'
+              /^16.*$/:
+              {
+                $daemon_directory_default='/usr/lib/postfix/sbin'
+                $postfix_ver='3.1.0'
+                $compatibility_level_default=undef
+              }
+              /^18.*$/:
+              {
+                $daemon_directory_default='/usr/lib/postfix/sbin'
+                $postfix_ver='3.3.0'
+                $compatibility_level_default=2
+              }
+              /^20.*$/:
+              {
+                $daemon_directory_default='/usr/lib/postfix/sbin'
+                $postfix_ver='3.4.10'
+                $compatibility_level_default=2
+              }
+              /^22.*$/:
+              {
+                $daemon_directory_default='/usr/lib/postfix/sbin'
+                $postfix_ver='3.6.4'
+                $compatibility_level_default=2
+              }
+              /^24.*$/:
+              {
+                $daemon_directory_default='/usr/lib/postfix/sbin'
+                $postfix_ver='3.6.4'
+                $compatibility_level_default=2
+              }
+              default: { fail("Unsupported Ubuntu version! - ${facts['os']['release']['full']}") }
             }
           }
-          else
-          {
-            $postfix_username_uid_default = $::eyp_postfix_uid ? {
-              undef   => '89',
-              default => $::eyp_postfix_uid,
-            }
-            $postfix_username_gid_default = $::eyp_postfix_gid ? {
-              undef   => '89',
-              default => $::eyp_postfix_gid,
-            }
-          }
-
-          case $::operatingsystemrelease
-          {
-            /^14.*$/:
-            {
-              $daemon_directory_default='/usr/lib/postfix'
-              $postfix_ver='2.11.0'
-              $compatibility_level_default=undef
-            }
-            /^16.*$/:
-            {
-              $daemon_directory_default='/usr/lib/postfix/sbin'
-              $postfix_ver='3.1.0'
-              $compatibility_level_default=undef
-            }
-            /^18.*$/:
-            {
-              $daemon_directory_default='/usr/lib/postfix/sbin'
-              $postfix_ver='3.3.0'
-              $compatibility_level_default=2
-            }
-            /^20.*$/:
-            {
-              $daemon_directory_default='/usr/lib/postfix/sbin'
-              $postfix_ver='3.4.10'
-              $compatibility_level_default=2
-            }
-            /^22.*$/:
-            {
-              $daemon_directory_default='/usr/lib/postfix/sbin'
-              $postfix_ver='3.6.4'
-              $compatibility_level_default=2
-            }
-            /^24.*$/:
-            {
-              $daemon_directory_default='/usr/lib/postfix/sbin'
-              $postfix_ver='3.6.4'
-              $compatibility_level_default=2
-            }
-            default: { fail("Unsupported Ubuntu version! - ${::operatingsystemrelease}")  }
-          }
+          'Debian': { fail('Unsupported') }
+          default: { fail('Unsupported Debian flavour!') }
         }
-        'Debian': { fail('Unsupported')  }
-        default: { fail('Unsupported Debian flavour!')  }
       }
-    }
-    'Suse':
-    {
-      $setgid_group_default='maildrop'
-      $compatibility_level_default=undef
-
-      $manage_mastercf_default=false
-      $daemon_directory_default='/usr/lib/postfix'
-      #$dependencies=['dpkg', 'grep' ]
-      $switch_to_postfix=undef
-      $check_postfix_mta=undef
-
-      $purge_default_mta=[ 'sendmail' ]
-
-      $mailclient=[ 'mailx' ]
-
-      $readme_directory_default=false
-
-      $postfix_username_uid_default='51'
-      $postfix_username_gid_default='51'
-
-      case $::operatingsystem
+      'Suse':
       {
-        'SLES':
-        {
-          case $::operatingsystemrelease
+        $setgid_group_default='maildrop'
+        $compatibility_level_default=undef
+
+        $manage_mastercf_default=false
+        $daemon_directory_default='/usr/lib/postfix'
+        #$dependencies=['dpkg', 'grep' ]
+        $switch_to_postfix=undef
+        $check_postfix_mta=undef
+
+        $purge_default_mta= ['sendmail']
+
+        $mailclient= ['mailx']
+
+        $readme_directory_default=false
+
+        $postfix_username_uid_default='51'
+        $postfix_username_gid_default='51'
+
+        case $facts['os']['name'] {
+          'SLES':
           {
-            '11.3':
-            {
-              $postfix_ver='2.9.4'
+            case $facts['os']['release']['full'] {
+              '11.3':
+              {
+                $postfix_ver='2.9.4'
+              }
+              /^12.[34]/:
+              {
+                $postfix_ver='3.2.0'
+              }
+              default: { fail("Unsupported operating system ${facts['os']['name']} ${facts['os']['release']['full']}") }
             }
-            /^12.[34]/:
-            {
-              $postfix_ver='3.2.0'
-            }
-            default: { fail("Unsupported operating system ${::operatingsystem} ${::operatingsystemrelease}") }
           }
+          default: { fail("Unsupported operating system ${facts['os']['name']}") }
         }
-        default: { fail("Unsupported operating system ${::operatingsystem}") }
       }
-    }
-    default: { fail('Unsupported OS!')  }
+      default: { fail("Unsupported OS! - ${facts['os']}.to_yaml") }
   }
-
 }
